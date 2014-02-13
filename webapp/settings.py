0dc0f08a2fa89576bfcb3bae44c4d0dd556351cd
Lucas Rondenet <lrondenet@gmail.com>
2014-02-12 17:39:56 -0800

    changed settings.py

--- webapp/settings.py
+++ webapp/settings.py
@@ -55,22 +55,12 @@ WSGI_APPLICATION = 'webapp.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/dev/ref/settings/#databases
 
-# Parse database configuration from $DATABASE_URL
-import dj_database_url
-DATABASES['default'] =  dj_database_url.config()
-
-# Honor the 'X-Forwarded-Proto' header for request.is_secure()
-SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
-
-# Allow all host headers
-ALLOWED_HOSTS = ['*']
-
-'''DATABASES = {
+DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
-}'''
+}
 
 # Internationalization
 # https://docs.djangoproject.com/en/dev/topics/i18n/
