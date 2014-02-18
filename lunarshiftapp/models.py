e627f63a19da3516ca2ae3a958445c95a3487037
josh <josh@josh-VirtualBox.(none)>
2014-02-18 13:13:39 -0800

    added model data

--- lunarshiftapp/models.py
+++ lunarshiftapp/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+
+class Person(models.Model):
+	name = models.CharField(max_length=100)
+	email  = models.CharField(max_length=50)
+	password = models.CharField(max_length=200)
+
+class Managers(models.Model):
+	name = models.CharField(max_length=100)
+	members = models.ManyToManyField(Person)
+
+class Staff(models.Model):
+	name = models.CharField(max_length=100)
+	members = models.ManyToManyField(Person)
+	availableHours = 
+
+class Schedule(models.Model):
