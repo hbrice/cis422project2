d02af59abe574002ebcf137c75a5765225762c10
joshschmidt <joshuas42@gmail.com>
2014-02-23 12:44:46 -0800

    added schedules

--- lunarshiftapp/models.py
+++ lunarshiftapp/models.py
@@ -1,11 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import User
+
 
 # Create your models here.
 
 class Person(models.Model):
-	name = models.CharField(max_length=100)
-	email  = models.CharField(max_length=50)
-	password = models.CharField(max_length=200)
+	company = models.CharField(max_length=100)
+	user = models.ForeignKey(User, unique=True)
+	availableTimes = Schedule
 
 class Managers(models.Model):
 	name = models.CharField(max_length=100)
@@ -14,6 +16,9 @@ class Managers(models.Model):
 class Staff(models.Model):
 	name = models.CharField(max_length=100)
 	members = models.ManyToManyField(Person)
-	availableHours = 
 
 class Schedule(models.Model):
+	#matrix = [[0 for x in xrange(24)] for x in xrange(7)] 
+
+class Schedules(models.Model):
+	
