8181bd7074e199686ad420c8add3731250fd91d5
Dillon <melanclock@darkstar.example.net>
2014-02-27 14:31:50 -0800

    Added a bit of skeleton code based on the Module Interface Specification

--- lunarshiftapp/models.py
+++ lunarshiftapp/models.py
@@ -1,9 +1,20 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+"""
+Employee Inherits from User:
+1. Instantiate a new employee consisting of a first name, last name, employeeID, email address, and a list of available times	
+2. Add and remove available dates from the list of available time.
+"""
 class Employee(models.Model):
 	company = models.CharField(max_length=100)
 	user = models.ForeignKey(User, unique=True)
+        
+        #Adds the DateTimeRange object to the list previously created by the Employee __init__ method.
+        def addTime(rng):
+
+        #Removes the DateTimeRange object at position index of the list of available times.
+        def removeTime(rng):
 
 class Manager(models.Model):
 	employee = models.ForeignKey(Employee)
