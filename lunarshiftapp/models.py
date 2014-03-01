6618740c9fe88e91485a95b16edb9c8415ec2c57
Tommy Dodson <t3.dodson@gmail.com>
2014-03-01 14:27:18 -0800

    fix datetimefields

--- lunarshiftapp/models.py
+++ lunarshiftapp/models.py
@@ -24,13 +24,13 @@ class Manager(models.Model):
 
 class Schedules(models.Model):
 	userID = models.ForeignKey(User, unique=True)
-	start = DateTimeField('%m/%d/%Y %H:%M') #format '10/25/2014 13:00'
-	end = DateTimeField('%m/%d/%Y %H:%M') #format '10/25/2014 13:00'
+	start = models.DateTimeField('%m/%d/%Y %H:%M') #format '10/25/2014 13:00'
+	end = models.DateTimeField('%m/%d/%Y %H:%M') #format '10/25/2014 13:00'
 	scheduleID = models.IntegerField(default=0)
 
 class Availability(models.Model):
 	userID = models.ForeignKey(User, unique=True)
-	start = DateTimeField('%m/%d/%Y %H:%M') #format '10/25/2014 13:00'
-	end = DateTimeField('%m/%d/%Y %H:%M') #format '10/25/2014 13:00'
+	start = models.DateTimeField('%m/%d/%Y %H:%M') #format '10/25/2014 13:00'
+	end = models.DateTimeField('%m/%d/%Y %H:%M') #format '10/25/2014 13:00'
 	isAvailable = models.BooleanField(initial=True)
 
