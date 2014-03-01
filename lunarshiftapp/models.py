78324df6da433f74d0d2b1f102c31a6e9de3a348
Lucas Rondenet <lrondenet@gmail.com>
2014-03-01 14:42:38 -0800

    fixing model.py

--- lunarshiftapp/models.py
+++ lunarshiftapp/models.py
@@ -1,18 +1,5 @@
 from django.db import models
 from django.contrib.auth.models import User
-from django import forms
-
-"""
-Employee Inherits from User:
-1. Instantiate a new employee consisting of a first name, last name, employeeID, email address, and a list of available times	
-2. Add and remove available dates from the list of available time.
-"""
-        
-        '''#Adds the DateTimeRange object to the list previously created by the Employee __init__ method.
-        def addTime(rng):
-
-        #Removes the DateTimeRange object at position index of the list of available times.
-        def removeTime(rng):'''
 
 class Manager(models.Model):	
 	company = models.CharField(max_length=100)
@@ -22,16 +9,3 @@ class Employee(models.Model):
 	manager = models.ForeignKey(Manager)
 	company = models.CharField(max_length=100)
 	user = models.ForeignKey(User, unique=True)
-
-'''class Schedules(models.Model):
-	userID = models.ForeignKey(User, unique=True)
-	start = models.DateTimeField('%m/%d/%Y %H:%M') #format '10/25/2014 13:00'
-	end = models.DateTimeField('%m/%d/%Y %H:%M') #format '10/25/2014 13:00'
-	scheduleID = models.IntegerField(default=0)
-
-class Availability(models.Model):
-	userID = models.ForeignKey(User, unique=True)
-	start = models.DateTimeField('%m/%d/%Y %H:%M') #format '10/25/2014 13:00'
-	end = models.DateTimeField('%m/%d/%Y %H:%M') #format '10/25/2014 13:00'
-	isAvailable = models.BooleanField(initial=True)'''
-
