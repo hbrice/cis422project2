ab0f6d40d2bf7a25a8ecc2c4b3e26562d53fc1e4
Lucas Rondenet <lrondenet@gmail.com>
2014-03-01 15:47:05 -0800

    fixing stuff

--- lunarshiftsite/settings.py
+++ lunarshiftsite/settings.py
@@ -36,8 +36,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'bootstrap3',
-    'South',
+    'south',
     'lunarshiftapp'
 )
 
