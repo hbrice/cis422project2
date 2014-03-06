31ef14e17b576327577ff3631e737fbf43eed7c5
Tommy Dodson <t3.dodson@gmail.com>
2014-03-06 15:50:13 -0800

    view stuff

--- lunarshiftapp/views.py
+++ lunarshiftapp/views.py
@@ -2,3 +2,11 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
+
+from models import Employee
+
+def index(request):
+	return HttpResponse("Hello, world. You're at the polls index.")
+    #latest_question_list = 
+    #context = 
+    #return render(request, 'polls/index.html', context)
