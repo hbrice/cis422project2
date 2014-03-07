8558464ac309cd10af456af5022f2e249037f318
Tommy Dodson <t3.dodson@gmail.com>
2014-03-06 16:57:48 -0800

    asdf

--- lunarshiftsite/urls.py
+++ lunarshiftsite/urls.py
@@ -1,13 +1,18 @@
 from django.conf.urls import patterns, include, url
 
 from django.contrib import admin
-import ../lunarshiftapp/views
+
+
+import os
 admin.autodiscover()
+
+
+print os.getcwd()
 
 urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'lunarshiftsite.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-	url(r'^$', views.index, name='index'),
+	#url(r'^$', views.index, name='index'),
     url(r'^admin/', include(admin.site.urls)),
 )
