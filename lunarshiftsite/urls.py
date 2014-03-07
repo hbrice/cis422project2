93899c169e6a96046b01878abe06819e299b9ed8
Tommy Dodson <t3.dodson@gmail.com>
2014-03-06 17:11:49 -0800

    reverting urls.py

--- lunarshiftsite/urls.py
+++ lunarshiftsite/urls.py
@@ -2,17 +2,13 @@ from django.conf.urls import patterns, include, url
 
 from django.contrib import admin
 
-
-import os
 admin.autodiscover()
 
-
-print os.getcwd()
 
 urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'lunarshiftsite.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-	#url(r'^$', views.index, name='index'),
+	# url(r'^$', views.index, name='index'),
     url(r'^admin/', include(admin.site.urls)),
 )
