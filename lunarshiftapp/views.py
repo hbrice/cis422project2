64fc59358bed9ae3df1cae12abd2b3d7340c21eb
Lucas Rondenet <lrondenet@gmail.com>
2014-03-06 21:14:18 -0800

    fixing index page

--- lunarshiftapp/views.py
+++ lunarshiftapp/views.py
@@ -6,7 +6,7 @@ from django.http import HttpResponse
 from models import Employee
 
 def index(request):
-	return HttpResponse("Hello, world. You're at the polls index.")
+	return HttpResponse("Hello, world. You're at the lunarshiftapp index.")
     #latest_question_list = 
     #context = 
     #return render(request, 'polls/index.html', context)
--- lunarshiftsite/urls.py
+++ lunarshiftsite/urls.py
@@ -1,7 +1,6 @@
 from django.conf.urls import patterns, include, url
 
 from django.contrib import admin
-
 admin.autodiscover()
 
 
@@ -10,5 +9,6 @@ urlpatterns = patterns('',
     # url(r'^$', 'lunarshiftsite.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 	# url(r'^$', views.index, name='index'),
+		url(r'^lunarshiftapp/', include('lunarshiftapp.urls')),
     url(r'^admin/', include(admin.site.urls)),
 )
