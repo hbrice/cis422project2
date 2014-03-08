6e102671eefc7d35e57fff34e4130b46528aa6a1
Lucas Rondenet <lrondenet@gmail.com>
2014-03-08 13:56:01 -0800

    working on login by username

--- lunarshiftapp/views.py
+++ lunarshiftapp/views.py
@@ -1,13 +1,36 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, authenticate, logout
+from django.contrib.auth.models import User
+from lunarshiftapp.models import Employee, Availibity, Schedule
+
+# mainly for testing routing using
 from django.http import HttpResponse
 
 # Create your views here.
-
-from models import Employee
-
-def index(request):
-	#return HttpResponse("This is where the signup page will be...")
+def index(request, auth_form=None, user_form=None):
 	return render(request,'login.html')
+	# This was for testing
+	# return HttpResponse("This is where the signup page will be...")
+
+def login_view(request):
+	if request.method == 'POST':
+		email = request.POST['email']
+		password = request.POST['password']
+		user = authenticate(username=username, password=password)
+		if user is not None:
+			if user.is_active:
+				login(request, user)
+				# redirect to user page...successful login
+				tmp = email + " " + password		
+				return HttpResponse(tmp)
+			else:
+				# return a disabled account error message
+				return HttpResponse("Error: disabled account")
+		else:
+			# invalid username/ password
+			return HttpResponse("username or password is incorrect")
+		
+	
 
 '''
 This will break code since url aren't defined yet...to do later
--- lunarshiftsite/urls.py
+++ lunarshiftsite/urls.py
@@ -10,5 +10,6 @@ urlpatterns = patterns('',
     # url(r'^blog/', include('blog.urls')),
 	# url(r'^$', views.index, name='index'),
 		url(r'^$', include('lunarshiftapp.urls')),
+		url(r'^login$', 'lunarshiftapp.views.login_view'),
     url(r'^admin/', include(admin.site.urls)),
 )
