1c00200cfa5bfa902b8749745a26bd2ef6a8a295
joshschmidt <joshuas42@gmail.com>
2014-03-09 15:10:50 -0700

    added two  models

--- lunarshiftapp/admin.py
+++ lunarshiftapp/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
-from lunarshiftapp.models import Employee
+from lunarshiftapp.models import Employee, Availibity, Schedule
 
 admin.site.register(Employee)
+admin.site.register(Availibity)
+admin.site.register(Schedule)
