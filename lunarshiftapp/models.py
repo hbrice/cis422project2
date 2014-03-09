43dc86d53c294b0f15aa399c6fe7225b5a9b040c
Tommy Dodson <t3.dodson@gmail.com>
2014-03-09 15:47:39 -0700

    migrating data_models to models

--- lunarshiftapp/models.py
+++ lunarshiftapp/models.py
@@ -25,6 +25,45 @@ class Employee(models.Model):
 		avail =  ' and has set there availibity ' + str(self.setAvailibity)
 		output = work + '\n' + avail
 		return output
+		
+	#The following fuctions are only applicable if Employee Manager
+	def selectEmployee(self, emp):
+		"""
+			Employee emp: Employee to select
+		"""
+		computeSchedules()
+		
+	def unselectEmployee(self, emp):
+		"""
+			Employee emp: Employee to deselect
+		"""
+		computeSchedules()
+	def activateEmployee(self, emp):
+		"""
+			Employee emp: Employee to activate
+		"""
+	
+	def deactivateEmployee(self, emp):
+		"""
+			Employee emp: Employee to deactivate
+		"""
+		
+	def addCoverage(self, times):
+		"""
+			DateTimeRange times: times to add to coverage
+		"""
+		computeSchedules()
+	def removeCoverage(self, times):
+		"""
+			DateTimeRange times: times to remove from coverage
+		"""
+		computeSchedules()
+	def clearSchedules(self):
+		"""
+			removes list of potential schedules and clears the best schedule
+		"""
+		self.bestSchedule = None
+		self.potentialSchedules = []
 
 class Availibity(models.Model):
 	user = models.ForeignKey(User)
