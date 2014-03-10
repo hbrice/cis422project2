cf8d3ca16bd937771e453d71ae2bcc10e338bfe6
melanclock <aether3330@gmail.com>
2014-03-10 12:58:59 -0700

    ehhh

--- lunarshiftapp/testSettings.py
+++
@@ -1,8 +0,0 @@
-import dj_database_url
-
-TEST_DATABASES = {
-    'default': dj_database_url.config(env='TEST_DATABASE_URL')
-}
-
-# replace path below to point to HerokuTestSuiteRunner class
-TEST_RUNNER = './lunarshiftapp/'# 'python.path.to.test_suite_runner.HerokuTestSuiteRunner'
