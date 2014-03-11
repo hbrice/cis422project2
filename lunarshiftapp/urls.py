68c7fddf69ecea564b1d9dc83be722f147e4b9f1
Lucas Rondenet <lrondenet@gmail.com>
2014-03-11 15:08:07 -0700

    trying to get dajax working

--- lunarshiftapp/urls.py
+++ lunarshiftapp/urls.py
@@ -1,11 +1,7 @@
-from django.conf.urls import patterns, include, url
-
-from dajaxice.core import dajaxice_autodiscover, dajaxice_config
-dajaxice_autodiscover()
+from django.conf.urls import patterns, url
 
 from lunarshiftapp import views
 
 urlpatterns = patterns('',
 	url(r'^$', views.index, name='index'),
-	url(dajaxice_config.dajaxice_url, include('dajaxice.urls')),
 )
