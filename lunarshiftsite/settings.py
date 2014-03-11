07738ff595d6e305b15f3991dc8e2407368ce293
joshschmidt <joshuas42@gmail.com>
2014-03-11 02:14:11 -0700

    adding database updates

--- lunarshiftsite/settings.py
+++ lunarshiftsite/settings.py
@@ -64,7 +64,6 @@ INSTALLED_APPS = (
 
 MIDDLEWARE_CLASSES = (
     'django.middleware.csrf.CsrfViewMiddleware',
-    'django.middleware.csrf.CsrfResponseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
