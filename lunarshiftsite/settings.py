17418332b742b9b43ed7e45478796000ca75ca83
melanclock <aether3330@gmail.com>
2014-03-10 18:25:23 -0700

    >.>

--- lunarshiftsite/settings.py
+++ lunarshiftsite/settings.py
@@ -119,4 +119,4 @@ TEST_DATABASES = {
 }
 
 # replace path below to point to HerokuTestSuiteRunner class
-TEST_RUNNER = 'lunarshiftsite.HerokuTestSuiteRunner' # python.path.to.test_suite_runner.HerokuTestSuiteRunner'
+TEST_RUNNER = 'lunarshiftsite.test_suite_runner' # python.path.to.test_suite_runner.HerokuTestSuiteRunner'
