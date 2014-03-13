5de122745563ff0be996ea9e18115fc10d160253
joshschmidt <joshuas42@gmail.com>
2014-03-13 10:05:51 -0700

    testing update

--- lunarshiftapp/views.py
+++ lunarshiftapp/views.py
@@ -194,11 +194,11 @@ def computeSchedule(request):
 				#		tmpSet.remove(x)
 				if len(tmpSet) == 0:
 					return None
-				else:
+				#else:
 					#return HttpResponse(tmpSet)
 					#newSchedule = Schedule(user=tmpSet[0].user,AvailibleDay=day,start_time=hour,end_time=hour+1)
 					#newSchedule.Save()
-	return HttpResponse("Schedules!" + numberOfHours)
+		 return HttpResponse("Schedules!" + numberOfHours)
 				
 				
 	return HttpResponse("No schedules!")
