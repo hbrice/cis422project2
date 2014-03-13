e8f236f7281dd1329365afb2068e3f4e8a3ac6ed
joshschmidt <joshuas42@gmail.com>
2014-03-13 08:41:26 -0700

    testing update

--- lunarshiftapp/functionality.py
+++ lunarshiftapp/functionality.py
@@ -112,7 +112,9 @@ class Alternative:
 				#get the number of total hours in the schedule that need coverage
 				numberOfHours += 1
 				#get the set of employees who can cover this hour
-				poolOfEmployees=Availibity.objects.filter(AvailibleDay=day,date__range=[day.start_time.hour,day.end_time.hour])
+				for e in Availibity.objects.filter(AvailibleDay=day):
+					if e.start_time.hour>= hour and e.end_time.hour < hour:
+						poolOfEmployees.append(e)
 				tmpSet = employeeSet
 				for x in tmpSet:
 					if x.name not in tmpSet:
