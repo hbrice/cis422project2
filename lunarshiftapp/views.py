9f5d27aa394740cb0a4d2d786a0131175bc4fdb1
joshschmidt <joshuas42@gmail.com>
2014-03-13 09:26:19 -0700

    testing update

--- lunarshiftapp/views.py
+++ lunarshiftapp/views.py
@@ -187,7 +187,7 @@ def computeSchedule(request):
 					#	poolOfEmployees.append(e)
 				poolOfEmployees =  Availibity.objects.filter(AvailibleDay=day)
 				tmpSet = employeesSet
-				return HttpResponse(len(Availibity.objects.filter(AvailibleDay=day)))
+				return HttpResponse(Availibity.objects.filter(AvailibleDay=day).count())
 				for x in poolOfEmployees:
 					if x.name not in tmpSet:
 						tmpSet.remove(x)
