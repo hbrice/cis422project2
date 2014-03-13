8f2e422001acd3c9439ad15d6ed21a6db901a52b
Tommy Dodson <t3.dodson@gmail.com>
2014-03-13 00:37:40 -0700

    some message

--- lunarshiftapp/functionality.py
+++ lunarshiftapp/functionality.py
@@ -51,13 +51,13 @@ class ScheduleStruct:
 
 
 		#loop through each day that has coverage
-		for day in daysNeedingCoverage
+		for day in daysNeedingCoverage:
 			#loop through each hour of coverage in the day
-			for hour in range(day.start_time, day.end_time)
+			for hour in range(day.start_time.hour, day.end_time.hour):
 				#get the number of total hours in the schedule that need coverage
 				numberOfHours += 1
 				#get the set of employees who can cover this hour
-				poolOfEmployees=Availibity.objects.get(AvailibleDay=day, start_time<=hour<=end_time))
+				poolOfEmployees=Availibity.objects.get(AvailibleDay=day).filter(date_range=[start_time,end_time])
 				for x in poolOfEmployees:
 					if x.name not in employeesSet:
 						employeeSet.remove(x)
@@ -72,35 +72,8 @@ class ScheduleStruct:
 	def getQueue(self):
 		queue = PriorityQueue(Employees.objects.all().len)
 		for day in self.sched.keys():
-			for hour in self.sched.values()
+			for hour in self.sched.values():
 				queue.put_nowait((self.sched[day][hour].len,self.sched[day][hour], day, hour)) #(num emps, emps[], day, hour)
 				
 		return queue
 		
-
-"""
-class DateTimeRange:
-	def __init__(self, day, start, end):
-		"""
-			#day is a string as defined above for the day.
-			#start and end can be either integers(representing hours) or time objects.
-			#creates tuple self.time (day, start, end)
-		"""
-		startTime = datetime.time(start)
-		endTime = datetime.time(end)
-		self.time = (day,startTime,endTime)
-	
-	def isDisjoint(self, otherRng):
-		"""
-			#DateTimeRange otherRng: other DateTimeRange
-			#returns bool whether self and other are disjoint
-		"""
-		return self.time[0] != otherRng.time[0] or ((self.time[1] < otherRng.time[1]) and (self.time[2] < otherRng.time[1])) or ((otherRng.time[1] < self.time[1]) and (otherRng.time[2] < self.time[1]))
-		
-	def hours(self):
-		"""
-			#returns a list of consecutive hours between start and end
-		"""
-		return List(range(self.time[1].hour,self.time[2].hour + 1))
-		
-"""
