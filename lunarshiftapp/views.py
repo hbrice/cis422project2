e396d79002d68e08b467b40c8ec382e80c18f3e0
joshschmidt <joshuas42@gmail.com>
2014-03-13 11:03:47 -0700

    testing update

--- lunarshiftapp/views.py
+++ lunarshiftapp/views.py
@@ -198,8 +198,8 @@ def computeSchedule(request):
 					return None
 				else:
 					#return HttpResponse(tmpSet)
-					newStart = hour + ":00" 
-					newEnd = hour+1 + ":00" 
+					newStart = str(hour) + ":00" 
+					newEnd = str(hour+1) + ":00" 
 					newSchedule = Schedule(user=User.objects.get(username='schmidtj'),AvailibleDay=day.AvailibleDay,start_time=newStart,end_time=newEnd)
 					newSchedule.save()
 					return HttpResponse()
